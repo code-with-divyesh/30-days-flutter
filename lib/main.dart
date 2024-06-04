a35d2659ef540ee2894ee7a79117634e6c32b214
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2024-06-04 15:20:54 +0530

    day 4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:catlogapp/pages/home.dart";
 import "package:flutter/material.dart";
 import 'package:catlogapp/pages/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,11 +15,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       // home: myHome(),
+      // initialRoute: "/home",
       routes: {
         "/": (context) => Login_page(),
         "/home": (context) => myHome(),
         "/loginpage": (context) => Login_page(),
       },
+      theme: ThemeData(fontFamily: GoogleFonts.lato().fontFamily),
     );
   }
 }
